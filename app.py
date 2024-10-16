de367bf14cbeeb9b2b64c8a39be864cc6827c9df
Amrit Regmi <107292672+amritregmi26@users.noreply.github.com>
2024-10-16 05:52:43 +0545

    update app.py

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
 app.config["MAIL_SERVER"] = 'smtp.gmail.com'
 app.config["MAIL_PORT"] = 465
 app.config["MAIL_USE_SSL"] = True
-app.config["MAIL_USERNAME"] = "amritregmi19070002@gmail.com"
-app.config["MAIL_PASSWORD"] = "pcptowqxdcwpwxfg"
+app.config["MAIL_USERNAME"] = ""
+app.config["MAIL_PASSWORD"] = ""
 
 db = SQLAlchemy(app)
 
